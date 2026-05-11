9f1e47d90386917ca52c259471b69b0d84341603
NoName Entity <nonameentity@proton.me>
2026-05-11 21:43:25 +0200

    initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "tsup";
+
+const defaultConfig = {
+	dts: {
+		compilerOptions: {
+			ignoreDeprecations: "6.0" // при объявлении paths появляется тригерный baseUrl.
+		}
+	},
+	sourcemap: true,
+	minify: true,
+	clean: true,
+	splitting: false,
+	treeshake: true,
+	tsconfig: "./tsconfig.json",
+	external: ["@tanstack/react-query", "zod"]
+};
+
+export default defineConfig([
+	{
+		entry: ["src/index.ts"],
+		format: ["cjs"],
+		outDir: "dist/cjs",
+		...defaultConfig
+	},
+	{
+		entry: ["src/index.ts"],
+		format: ["esm"],
+		outDir: "dist/esm",
+		...defaultConfig
+	}
+]);
