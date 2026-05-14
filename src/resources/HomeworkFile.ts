eeec19afdc757773e201885c350f8761f5a88c9b
NoName Entity <nonameentity@proton.me>
2026-05-14 21:10:04 +0200

    mod: HomeworkFile.toid now linked with Homework.id

--- src/resources/HomeworkFile.ts
+++ src/resources/HomeworkFile.ts
@@ -1,9 +1,10 @@
 import z from "zod";
+import { Homework } from "./Homework";
 
 export const HomeworkFile = z.object({
 	filename: z.string(),
 	link: z.httpUrl(),
-	toid: z.number()
+	toid: Homework.shape.id
 });
 
 export type IHomeworkFile = z.input<typeof HomeworkFile>;
