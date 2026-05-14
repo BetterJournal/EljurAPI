5c4b7afb964ac606e483688472073fd85deb53de
NoName Entity <nonameentity@proton.me>
2026-05-14 23:24:04 +0200

    mod: fix types in HomeworkRecordModel.ts

--- src/diary/HomeworkRecordModel.ts
+++ src/diary/HomeworkRecordModel.ts
@@ -1,24 +1,23 @@
 import z from "zod";
-import { UserDataBase } from "@/utils";
+import { SubjectBase, UserDataBase } from "@/utils";
 import { Homework, HomeworkFile } from "@/resources";
 
-export const SubjectHomework = z.object({
+export const SubjectHomework = SubjectBase.extend({
 	files: z.array(HomeworkFile).optional(),
 	homework: z.record(
-		z.number(),
+		Homework.shape.id,
 		Homework.pick({
 			id: true,
 			individual: true,
 			value: true
 		})
-	),
-	name: z.string()
+	)
 });
 
 export type ISubjectHomework = z.input<typeof SubjectHomework>;
 
 export const HomeworkDay = z.object({
-	items: z.record(z.string(), SubjectHomework),
+	items: z.record(SubjectHomework.shape.name, SubjectHomework),
 	name: z.string(),
 	title: z.string()
 });
@@ -26,7 +25,7 @@ export const HomeworkDay = z.object({
 export type IHomeworkDay = z.input<typeof HomeworkDay>;
 
 export const HomeworkRecord = UserDataBase.extend({
-	days: z.record(z.string(), HomeworkDay)
+	days: z.record(HomeworkDay.shape.name, HomeworkDay)
 });
 
 export type IHomeworkRecord = z.input<typeof HomeworkRecord>;
